f627889382643fc677ce645bcd7f6a128cc54d70
lightyears <lightyears1998@hotmail.com>
2018-11-06 09:43:17 +0800

    创建项目的骨架

---
+++ script.js
@@ -0,0 +1,21 @@
+var roomId;
+
+// 生成房间编号
+function generateRoomId() {
+    
+}
+
+// 检查房间编号的有效性
+function checkRoomId() {
+    return false;
+}
+
+// 发送消息到服务器端
+function sendMessage() {
+    
+}
+
+// 从服务器端请求数据
+function requestMessage() {
+    
+}

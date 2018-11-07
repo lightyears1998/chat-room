0ea873a141606c195b1e4ba318fa393c4298947e
lightyears <lightyears1998@hotmail.com>
2018-11-07 20:59:30 +0800

    完善后端
    希望不会被XSS

--- backend.php
+++ backend.php
@@ -1,13 +1,55 @@
 <?php
 
-if ($_REQUEST['room'] != null) {
-	$room = hash('sha1', $_REQUEST['room']);  // 获取房间暗号对应Sha1值
+/** 服务器端文件结构
+ *
+ * /- 根目录
+ *  | hash_room - 房号对应的sha1值；
+ *              | message_count - 服务器上保存消息的总数
+ *              | 1 - 第一条消息
+ *              | 2 - 第二条消息
+ *              | ...
+**/
+
+// 请求中必须具备房号和昵称信息，丢弃没有房号信息的请求
+if (isset($_REQUEST['room']) && isset($_REQUEST['nickname'])) {
+	$hash_room = hash('sha1', $_REQUEST['room']);  // 获取房号对应sha1值
+	$nickname = htmlspecialchars($_REQUEST['nickname']);
 	
 	// 如果房间哈希不存在,创建房间并添加第一条消息
-	if (!file_exists($room)) {
-		mkdir($room);
-		$count = fopen($room.'/totalMessageCount', 'w');
-		fwrite($count, 1);
-		fclose($count);
+	if (!file_exists($hash_room) && mkdir($hash_room)) {
+		// 创建保存消息总数的message_count文件,并将消息总数置为1
+		file_put_contents("${hash_room}/message_count", 1);
+		
+		// 生成建立房间的欢迎消息
+		$date = date('Y-m-d h:i:sa');
+		$content = "<b>${nickname}</b> <i>${date}</i> 创建房间";
+		
+		// 将欢迎消息写入1号文件
+		file_put_contents("${hash_room}/1", $content);
+	}
+	
+	// 向客户端返回服务器端的消息总数
+	if (isset($_REQUEST['localMessageCount'])) {
+		$message_count = file_get_contents("${hash_room}/message_count");
+		print($message_count);
+	}
+	
+	// 向客户端返回对应序号的消息
+	if (isset($_REQUEST['index'])) {
+		// 从文件中加载消息并返回给客户端
+		$index = intval($_REQUEST['index']);
+		$content = file_get_contents("${hash_room}/${index}");
+		print($content);
+	}
+	
+	// 将客户端发送的消息存入服务器
+	if (isset($_REQUEST['message'])) {
+		$message_count = intval(file_get_contents("${hash_room}/message_count")) + 1;
+		file_put_contents("${hash_room}/message_count", $message_count);
+		
+		$date = date('Y-m-d h:i:sa');
+		$message = htmlspecialchars($_REQUEST['message']);
+		$content = "<b>${nickname}</b> <i>${date}</i> ${message}";
+		file_put_contents("${hash_room}/${message_count}", $content);
 	}
 }
--- script.js
+++ script.js
@@ -1,75 +1,107 @@
 const serverURL = 'backend.php';
 
-var room;       // 房号
-var nickname;   // 昵称
-var localMessageCount;          // 本地接收的消息总数
-var remoteMessageCount;         // 服务器上消息的总数
+var room;                       // 房号
+var nickname;                   // 昵称
+var ready = false;              // 在用户确认了房号和昵称信息之后为true
+var localMessageCount = 0;      // 本地接收的消息数量
+var serverMessageCount = 0;     // 服务器上消息的总数
 
-// 初始化页面
-// 从查询字符串中获取房号和昵称等信息
-// 将按钮的状态置为“使用昵称并加入房间”
+// 当页面加载完成时进行初始化工作
 $(function() {
+	// 从查询字符串中获取房间号和用户房间号和昵称等信息
 	const params = new URLSearchParams(document.location.search);
-	
 	room = params.get('room')
 	nickname = params.get('nickname');
-	localMessageCount = 0;
-	remoteMessageCount = 0;
 	
 	$('#room').val(room);
 	$('#nickname').val(nickname);
 	
-	setInterval(updateRemoteMessageCount, 2000);
+	// 若用户在会话中改变了房间号则暂停从服务器获取数据，等待用户确认
+	$('#room').change(pause);
+	$('#nickname').change(pause);
+	setInterval(updateMessageFromServer, 1000);  // 定时从服务器端获取消息
 });
+
+// 将网页设置为“等待用户确认信息”状态
+function pause() {
+	ready = false;
+	$('#message').prop('disabled', true);
+	$('#submit').html('加入房间');
+}
+
+// 将网页设置为“正常工作”状态
+function start() {
+	if (!($('#room').val() && $('#nickname').val())) {
+		alert('房间号和昵称都是必填项目哟');
+	}
+	if (room != $('#room').val()) {
+		$('#dialog').empty();
+		localMessageCount = 0;
+	}  // 若更改了房间号则清除历史聊天记录
+	room = $('#room').val();
+	nickname = $('#nickname').val();
+	$('#message').prop('disabled', false);
+	$('#submit').html('发送消息');
+	ready = true;
+}
 
 // 当Submit按钮被按下
 function onClickSubmitButton() {
-	room = $('#room').val();
-	nickname = $('#nickname').val();
-	
-	sendMessage();
+	if (ready && $('#message').val()) {
+		sendMessage();
+	}
+	if (!ready) {
+		start();
+	}
 }
 
 // 发送消息到服务器端
 function sendMessage() {
-	var data = {
-		room: room,
-		nickname: nickname,
-		message: $('#message').val()
-	};
-	$.post(serverURL, data);
+	if (ready) {
+		var data = {
+			room: room,
+			nickname: nickname,
+			message: $('#message').val()
+		};
+		$.post(serverURL, data);
+		$('#message').val('');
+	}
 }
 
 // 从服务器端更新消息总数
-function updateRemoteMessageCount() {
-    if (room != null && nickname != null) {
+function updateMessageFromServer() {
+    if (ready) {
 		var data = {
 			room: room,
+			nickname: nickname,
 			localMessageCount: localMessageCount
 		};
 		$.post(serverURL, data, function(response, status) {
 			if (status == 'success') {
-				$('#status').html(String(response));
-				remoteMessageCount = Number(response);
+				$('#status').html('共计' + String(response) + '条消息');
+				serverMessageCount = Number(response);
 			} else {
 				$('#status').html('通信质量不佳');
 			}
-			getMessage();
+			getNextMessage();
 		});
 	}
 }
 
-// 从服务器上获取消息
-function getMessage() {
-	if (localMessageCount < remoteMessageCount) {
+// 从服务器获取下一条尚未下载到本地的消息
+function getNextMessage() {
+	if (ready && localMessageCount < serverMessageCount) {
 		var data = {
 			room: room,
-			index: localMessageCount+1
+			nickname: nickname,
+ 			index: localMessageCount + 1
 		}
 		$.post(serverURL, data, function(response, status) {
-			$('#dialog').append('<p>' + String(response) + '</p>');
-			localMessageCount++;
-			getMessage()();
+			if (status == 'success') {
+				$('#dialog').append('<p>' + String(response) + '</p>');
+				localMessageCount++;
+				getNextMessage();
+			}
 		});
 	}
 }
